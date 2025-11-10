006bbf88732805294a3bd34e2f26c638185fb585
cvschmalz <teenyschool@gmail.com>
2025-11-10 14:26:27 +0000

    .

--- app.py
+++ app.py
@@ -9,9 +9,11 @@ APP = Flask(__name__)
 
 # # CPU usage per core
 # print("CPU:", psutil.cpu_percent(percpu=True)[0], "%")
+# cpu = psutil.cpu_percent(percpu=True)[0] + " %"
 
 # # Mem (MB)
 # print("Memória usada:", psutil.virtual_memory().used // 1024 ** 2, "MB")
+# mem = psutil.virtual_memory().used // 1024 ** 2 + " MB"
 
 # # Get process PID
 # print(os.getpid())
@@ -19,13 +21,16 @@ APP = Flask(__name__)
 
 # # OS
 # print("Sistema operacional:", platform.platform())
+# current_os = platform.platform()
 
 @APP.get("/info")
 def info():
     return json.dumps([
         {
             'integrantes': [
-                'Christine von Schmalz'
+                'Christine von Schmalz',
+                'Cecília Lucchesi Mardegan',
+                'Erick Maestri de Souza'
             ]
         }
     ])
@@ -34,7 +39,7 @@ def info():
 def metricas():
     process = psutil.Process(os.getpid())
     cpu_usage = psutil.cpu_percent(interval=0.1)
-    mem_usage = process.memory_info().rss / (1024 * 1024)  # MB
+    mem_usage = process.memory_info().rss / (1024 * 1024)
     pid = process.pid
     current_os = platform.system() + " (" + platform.release() + ")"
 
@@ -44,3 +49,6 @@ def metricas():
         "CPU": f"{cpu_usage:.2f}%",
         "Sistema Operacional": current_os
     })
+
+if __name__ == "__main__":
+    APP.run(host="0.0.0.0", port=5000)
