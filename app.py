ecc63ae8710d661101b573b07e72e79fd4c836e8
cvschmalz <teenyschool@gmail.com>
2025-11-10 14:03:19 +0000

    .

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
 from flask import Flask
+
+import os
+import psutil
+import platform
 import json
 
 APP = Flask(__name__)
+
+# CPU usage per core
+print("CPU:", psutil.cpu_percent(percpu=True)[0], "%")
+cpu = psutil.cpu_percent(percpu=True)[0] + " %"
+
+# Mem (MB)
+print("Memória usada:", psutil.virtual_memory().used // 1024 ** 2, "MB")
+mem = psutil.virtual_memory().used // 1024 ** 2 + " MB"
+
+# Get process PID
+print(os.getpid())
+pid = os.getpid()
+
+# OS
+print("Sistema operacional:", platform.platform())
+current_os = platform.platform()
 
 @APP.get("/info")
 def info():
@@ -10,5 +30,16 @@ def info():
             'integrantes': [
                 'Christine von Schmalz'
             ]
+        }
+    ])
+
+@APP.get("/metricas")
+def metricas(cpu, mem, pid, current_os):
+    return json.dumps([
+        {
+            "PID": pid,
+            "CPU": cpu,
+            "Memória usada": mem,
+            "Sistema operacional": current_os
         }
     ])
